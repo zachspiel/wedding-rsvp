9b5d6e4722859ababd5466bcc5ffc6aafd280e87
zachspiel <zachspielberger@gmail.com>
2024-03-04 08:31:04 -0700

    Add link to With Joy

--- src/features/Registry/index.tsx
+++ src/features/Registry/index.tsx
@@ -1,4 +1,4 @@
-import { Card, Text, Flex } from "@mantine/core";
+import { Card, Text, Flex, Anchor } from "@mantine/core";
 import { SectionContainer, SectionTitle } from "@spiel-wedding/common";
 import Image from "next/image";
 import classes from "./registry.module.css";
@@ -18,9 +18,19 @@ const Registry = (): JSX.Element => {
           gift!
         </Text>
         <Text mb="sm">
-          We&apos;re lucky to already have a home full of everything we need. If you wish
-          to contribute beyond your attendance, we will be accepting contributions to our
-          honeymoon and first home fund at our reception following the ceremony.
+          But for those of you who are stubborn,
+          <Anchor href="https://withjoy.com/sedona-and-zach/registry" target="_blank">
+            {" "}
+            we&apos;ve put together a wish-list to help you out
+          </Anchor>
+          . Our dream honeymoon would be in St. Lucia. Anything you can contribute is
+          greatly appreciated! We will also be accepting contributions to our honeymoon
+          fund at our reception following the ceremony.
+        </Text>
+        <Text mb="sm">
+          Please note that if you would like to purchase a physical item, you will need to
+          ship it to yourself and bring it to the reception or reach out to Sedona and
+          Zach for their address.
         </Text>
         <Text>Thanks for visiting and we can&apos;t wait to see you on our big day!</Text>
         <Flex justify="center">
