bd2a80fddb08ce834324d76d12ffc8ccfafd7eb1
zachspiel <zachspielberger@gmail.com>
2024-03-31 19:11:59 -0700

    Fix ordering of links for navbar and footer

--- src/components/common/links.ts
+++ src/components/common/links.ts
@@ -8,7 +8,7 @@ export const links: MenuItem[] = [
   { label: "When & Where", link: "#whenAndWhere" },
   { label: "Guest Book", link: "#guestBook" },
   { label: "Registry", link: "#registry" },
-  { label: "Gallery", link: "#gallery" },
   { label: "FAQ", link: "#faq" },
+  { label: "Gallery", link: "#gallery" },
   { label: "RSVP", link: "#rsvp" },
 ];
