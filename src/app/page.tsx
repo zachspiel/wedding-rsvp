2a897e321d14aee34e1b324cd47260b7b9adf2b1
Zachary Spielberger <zachspielberger@gmail.com>
2024-07-04 20:56:23 -0700

    Remove component that may cause hydration issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import { createClient } from "@spiel-wedding/database/server";
+import FAQ from "@spiel-wedding/features/FAQ";
 import Gallery from "@spiel-wedding/features/Gallery";
 import Jumbotron from "@spiel-wedding/features/Jumbotron";
+import RSVP from "@spiel-wedding/features/RSVP";
 import Registry from "@spiel-wedding/features/Registry";
-import WhenAndWhere from "@spiel-wedding/features/WhenAndWhere";
 import ZachAndSedona from "@spiel-wedding/features/ZachAndSedona";
 import { getEvents } from "@spiel-wedding/hooks/events";
 import { getFAQs } from "@spiel-wedding/hooks/faq";
@@ -35,11 +36,11 @@ export default async function Home() {
     <main>
       <Jumbotron />
       <ZachAndSedona />
-      <WhenAndWhere />
-      {/*<RSVP events={events} />
-       <GuestBook guestMessages={guestMessages} /> */}
+      {/* <WhenAndWhere /> */}
+      <RSVP events={events} />
+      {/*<GuestBook guestMessages={guestMessages} /> */}
       <Registry />
-      {/*<FAQ faqs={faqs} />*/}
+      <FAQ faqs={faqs} />
       <Gallery gallery={gallery} />
     </main>
   );
