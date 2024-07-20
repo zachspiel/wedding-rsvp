e0ab01c289330358d06a9279298377af1a40eb47
Zachary Spielberger <zachspielberger@gmail.com>
2024-07-20 11:08:46 -0700

    Move middleware to correct location

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,24 +1,14 @@
 import { Center, Container } from "@mantine/core";
 import { SectionTitle } from "@spiel-wedding/components/common";
-import { createClient } from "@spiel-wedding/database/server";
 import { getEvents } from "@spiel-wedding/hooks/events";
 import { getGroups } from "@spiel-wedding/hooks/guests";
-import { redirect } from "next/navigation";
 import EventSummary from "./components/EventSummary";
 
 export const revalidate = 0;
 
 async function getProps() {
-  const supabase = createClient();
-  const { data } = await supabase.auth.getUser();
-
-  if (!data?.user) {
-    return redirect("/");
-  }
-
   const [groups, events] = await Promise.all([getGroups(), getEvents()]);
-
-  return { user: data?.user, groups, events };
+  return { groups, events };
 }
 
 export default async function Events() {
